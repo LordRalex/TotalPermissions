17cdf9bb3a4a0c3758719188dc066fdbae526dbf
Lord_Ralex <lordralex@gmail.com>
2012-08-03 22:42:41 -0500

    Created command abstract class
    This is the abstract class for the subcommands

---
+++ src/com/lordralex/permissionsar/commands/subcommands/SubCommand.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lordralex.permissionsar.commands.subcommands;
+
+import org.bukkit.command.CommandSender;
+
+/**
+ *
+ * @author Joshua
+ */
+public abstract class SubCommand {
+
+    /**
+     * Returns the command's name. When used, it is the /par [name].
+     *
+     * @return Name of the command
+     */
+    public abstract String getName();
+
+    /**
+     * Returns a String showing how to use this command.
+     *
+     * @return The help statement
+     */
+    public abstract String getHelp();
+
+    /**
+     * Executes the command. Only the args and sender are needed.
+     */
+    public abstract void execute(CommandSender sender, String[] args);
+}
