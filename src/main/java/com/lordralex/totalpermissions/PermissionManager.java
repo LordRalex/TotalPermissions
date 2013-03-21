703caf985100665d467e18971e967e0124d3d62e
LordRalex <lordralex@gmail.com>
2013-03-21 14:15:15 -0500

    Overhaul permmanager

--- src/main/java/com/lordralex/totalpermissions/PermissionManager.java
+++ src/main/java/com/lordralex/totalpermissions/PermissionManager.java
@@ -23,9 +23,7 @@ import com.lordralex.totalpermissions.permission.PermissionUser;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
-import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -38,9 +36,9 @@ import org.bukkit.entity.Player;
  */
 public final class PermissionManager {
 
-    protected Map<String, PermissionGroup> groups = new ConcurrentHashMap<String, PermissionGroup>();
-    protected Map<String, PermissionUser> users = new ConcurrentHashMap<String, PermissionUser>();
-    protected String defaultGroup = null;
+    protected final Map<String, PermissionGroup> groups = new ConcurrentHashMap<String, PermissionGroup>();
+    protected final Map<String, PermissionUser> users = new ConcurrentHashMap<String, PermissionUser>();
+    protected String defaultGroup;
     protected PermissionConsole console;
     protected PermissionRcon remote;
 
@@ -64,7 +62,6 @@ public final class PermissionManager {
         if (defaultGroup == null) {
             throw new InvalidConfigurationException("You must define at least one default group");
         }
-        ConsoleCommandSender cmd = Bukkit.getConsoleSender();
         console = new PermissionConsole();
         remote = new PermissionRcon();
     }
@@ -72,6 +69,7 @@ public final class PermissionManager {
     public void unload() {
         users.clear();
         groups.clear();
+        TotalPermissions.getPlugin().getListener().clearPerms();
     }
 
     public String getDefaultGroup() {
@@ -85,7 +83,7 @@ public final class PermissionManager {
      *
      * @since 1.0
      */
-    public synchronized PermissionConsole getConsole() {
+    public PermissionConsole getConsole() {
         return console;
     }
 
@@ -96,7 +94,7 @@ public final class PermissionManager {
      *
      * @since 1.0
      */
-    public synchronized PermissionRcon getRcon() {
+    public PermissionRcon getRcon() {
         return remote;
     }
 
@@ -110,12 +108,15 @@ public final class PermissionManager {
      *
      * @since 1.0
      */
-    public synchronized PermissionUser getUser(String player) {
-        PermissionUser user = users.get(player.toLowerCase());
-        if (user == null) {
-            user = new PermissionUser(player);
+    public PermissionUser getUser(String player) {
+        PermissionUser user;
+        synchronized (users) {
+            user = users.get(player.toLowerCase());
+            if (user == null) {
+                user = new PermissionUser(player);
+            }
+            users.put(player.toLowerCase(), user);
         }
-        users.put(player.toLowerCase(), user);
         return user;
     }
 
@@ -129,13 +130,8 @@ public final class PermissionManager {
      *
      * @since 1.0
      */
-    public synchronized PermissionUser getUser(Player player) {
-        PermissionUser user = users.get(player.getName().toLowerCase());
-        if (user == null) {
-            user = new PermissionUser(player);
-        }
-        users.put(player.getName().toLowerCase(), user);
-        return user;
+    public PermissionUser getUser(Player player) {
+        return getUser(player.getName());
     }
 
     /**
@@ -147,12 +143,15 @@ public final class PermissionManager {
      *
      * @since 1.0
      */
-    public synchronized PermissionGroup getGroup(String name) {
-        PermissionGroup group = groups.get(name.toLowerCase());
-        if (group == null) {
-            group = new PermissionGroup(name);
+    public PermissionGroup getGroup(String name) {
+        PermissionGroup group;
+        synchronized (groups) {
+            group = groups.get(name.toLowerCase());
+            if (group == null) {
+                group = new PermissionGroup(name);
+            }
+            groups.put(name.toLowerCase(), group);
         }
-        groups.put(name.toLowerCase(), group);
         return group;
     }
 
@@ -162,79 +161,95 @@ public final class PermissionManager {
      * @param perm
      * @return True if player has been given this perm, false otherwise
      */
-    public synchronized boolean has(Player player, String perm) {
+    public boolean has(Player player, String perm) {
         return has(player.getName(), perm);
     }
 
-    public synchronized boolean has(String player, String perm) {
+    public boolean has(String player, String perm) {
         String name = player.toLowerCase();
-        PermissionUser user = users.get(name);
-        if (user == null) {
-            user = new PermissionUser(name);
-            users.put(name, user);
+        PermissionUser user;
+        synchronized (users) {
+            user = users.get(name);
+            if (user == null) {
+                user = new PermissionUser(name);
+                users.put(name, user);
+            }
         }
         return user.has(perm);
     }
 
-    public synchronized boolean groupHas(String groupName, String perm) {
+    public boolean groupHas(String groupName, String perm) {
         String name = groupName.toLowerCase();
-        PermissionGroup group = groups.get(name);
-        if (group == null) {
-            group = new PermissionGroup(name);
-            groups.put(name, group);
+        synchronized (groups) {
+            PermissionGroup group = groups.get(name);
+            if (group == null) {
+                group = new PermissionGroup(name);
+                groups.put(name, group);
+            }
         }
         return false;
     }
 
-    public synchronized void addPerm(Player player, String perm, boolean allowance) {
+    public void addPerm(Player player, String perm, boolean allowance) {
         String name = player.getName().toLowerCase();
-        PermissionUser user = users.get(name);
-        if (user == null) {
-            user = new PermissionUser(name);
-            users.put(name, user);
+        synchronized (users) {
+            PermissionUser user = users.get(name);
+            if (user == null) {
+                user = new PermissionUser(name);
+                users.put(name, user);
+            }
+            if (!allowance) {
+                perm = "-" + perm;
+            }
+            user.addPerm(perm);
         }
-        if (!allowance) {
-            perm = "-" + perm;
-        }
-        user.addPerm(perm);
     }
 
     public synchronized void addPerm(OfflinePlayer player, String perm, boolean allowance) {
         String name = player.getName().toLowerCase();
-        PermissionUser user = users.get(name);
-        if (user == null) {
-            user = new PermissionUser(name);
-            users.put(name, user);
+
+        synchronized (users) {
+            PermissionUser user = users.get(name);
+            if (user == null) {
+                user = new PermissionUser(name);
+                users.put(name, user);
+            }
+            if (!allowance) {
+                perm = "-" + perm;
+            }
+            user.addPerm(perm);
         }
-        if (!allowance) {
-            perm = "-" + perm;
-        }
-        user.addPerm(perm);
     }
 
-    public synchronized void addPermToGroup(String group, String perm, boolean allowance) {
+    public void addPermToGroup(String group, String perm, boolean allowance) {
         String name = group.toLowerCase();
-        PermissionGroup gr = groups.get(name);
-        if (gr == null) {
-            gr = new PermissionGroup(name);
-            groups.put(name, gr);
+
+        synchronized (groups) {
+            PermissionGroup gr = groups.get(name);
+            if (gr == null) {
+                gr = new PermissionGroup(name);
+                groups.put(name, gr);
+            }
+            if (!allowance) {
+                perm = "-" + perm;
+            }
+            gr.addPerm(perm);
         }
-        if (!allowance) {
-            perm = "-" + perm;
-        }
-        gr.addPerm(perm);
     }
 
-    public synchronized void addPermToUser(String user, String perm, boolean allowance) {
+    public void addPermToUser(String user, String perm, boolean allowance) {
         String name = user.toLowerCase();
-        PermissionUser gr = users.get(name);
-        if (gr == null) {
-            gr = new PermissionUser(name);
-            users.put(name, gr);
+
+        synchronized (users) {
+            PermissionUser gr = users.get(name);
+            if (gr == null) {
+                gr = new PermissionUser(name);
+                users.put(name, gr);
+            }
+            if (!allowance) {
+                perm = "-" + perm;
+            }
+            gr.addPerm(perm);
         }
-        if (!allowance) {
-            perm = "-" + perm;
-        }
-        gr.addPerm(perm);
     }
 }
