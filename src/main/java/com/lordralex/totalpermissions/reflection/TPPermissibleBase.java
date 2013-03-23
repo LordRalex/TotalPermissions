3667149f5b0d979f1b173d13fcb4b0097d4ca799
LordRalex <lordralex@gmail.com>
2013-03-23 13:29:06 -0500

    Opps, forgot about this class

--- src/main/java/com/lordralex/totalpermissions/reflection/TPPermissibleBase.java
+++ src/main/java/com/lordralex/totalpermissions/reflection/TPPermissibleBase.java
@@ -42,7 +42,7 @@ public class TPPermissibleBase extends PermissibleBase {
         parent = p;
         Object obj = null;
         try {
-            Class cl = Class.forName("org.bukkit.craftbukkit.v1_4_R1.entity.CraftHumanEntity");
+            Class cl = Class.forName("org.bukkit.craftbukkit." + TotalPermissions.BUKKIT_VERSION + ".entity.CraftHumanEntity");
             Field field = cl.getDeclaredField("perm");
             field.setAccessible(true);
             obj = field.get(parent);
