7e5038a115974dc7548eca07704e7d519e6ad543
LordRalex <lordralex@gmail.com>
2013-05-20 13:05:01 -0500

    Remove thrown exception

--- src/main/java/net/ae97/totalpermissions/permission/PermissionGroup.java
+++ src/main/java/net/ae97/totalpermissions/permission/PermissionGroup.java
@@ -57,6 +57,5 @@ public class PermissionGroup extends PermissionBase {
     }
 
     public void setAsDefaultGroup() {
-        throw new UnsupportedOperationException("Not supported yet."); //Sets the current PermissionGroup as the default | Don't forget to remove the old default
     }
 }
